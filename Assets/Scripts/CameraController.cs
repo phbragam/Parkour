14409821ed74b8cae7d0eb22a100bb6a84d6f157
Pedro Braga <phbm.braga@gmail.com>
2022-07-03 18:00:28 -0300

    Camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform followTarget;
+
+    [SerializeField] float rotationSpeed = 2f;
+    [SerializeField] float distance = 5;
+    [SerializeField] float minVerticalAngle = -45;
+    [SerializeField] float maxVerticalAngle = 45;
+
+    [SerializeField] Vector2 framingOffSet;
+
+    [SerializeField] bool invertX;
+    [SerializeField] bool invertY;
+
+    float invertXVal;
+    float invertYVal;
+
+    float rotationX;
+    float rotationY;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        invertXVal = (invertX) ? -1 : 1;
+        invertYVal = (invertY) ? -1 : 1;
+
+        rotationX += Input.GetAxis("Mouse Y") * rotationSpeed * invertYVal;
+        rotationX = Mathf.Clamp(rotationX, minVerticalAngle, maxVerticalAngle);
+
+        rotationY += Input.GetAxis("Mouse X") * rotationSpeed * invertXVal;
+
+        var targetRotation = Quaternion.Euler(rotationX, rotationY, 0);
+
+        var focusPosition = followTarget.position + (Vector3)framingOffSet;
+
+        transform.position = focusPosition - targetRotation * new Vector3(0, 0, distance);
+        transform.rotation = targetRotation;
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Parkour system in Unity
+
+## Description
+
+Parkour system made following [this tutorial](https://www.udemy.com/course/unity-parkour-system/)
+
+### Unity Version
+
+Developed with Unity 2020.3.24f1 
