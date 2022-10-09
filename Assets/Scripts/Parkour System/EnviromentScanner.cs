2353b125cc7b25c4a0ba9e78931a7ee59be94005
Pedro Braga <phbm.braga@gmail.com>
2022-10-09 12:19:20 -0300

    Obstacle Height Detection

--- Assets/Scripts/Parkour System/EnviromentScanner.cs
+++ Assets/Scripts/Parkour System/EnviromentScanner.cs
@@ -6,6 +6,7 @@ public class EnviromentScanner : MonoBehaviour
 {
     [SerializeField] Vector3 forwardRayOffset = new Vector3(0, 0.25f, 0);
     [SerializeField] float forwardRayLength = 0.8f;
+    [SerializeField] float heightRayLength = 5;
     [SerializeField] LayerMask obstacleLayer;
 
     public ObstacleHitData ObstacleCheck()
@@ -19,6 +20,16 @@ public class EnviromentScanner : MonoBehaviour
         Debug.DrawRay(forwardOrigin, transform.forward * forwardRayLength,
             (hitData.forwardHitFound) ? Color.red : Color.white);
 
+        if (hitData.forwardHitFound == true)
+        {
+            var heightOrigin = hitData.forwardHit.point + Vector3.up * heightRayLength;
+            hitData.heightHitFound = Physics.Raycast(heightOrigin, Vector3.down,
+                out hitData.heightHit, heightRayLength, obstacleLayer);
+
+            Debug.DrawRay(heightOrigin, Vector3.down * heightRayLength,
+                (hitData.heightHitFound) ? Color.red : Color.white);
+        }
+
         return hitData;
     }
 }
@@ -26,5 +37,7 @@ public class EnviromentScanner : MonoBehaviour
 public struct ObstacleHitData
 {
     public bool forwardHitFound;
+    public bool heightHitFound;
     public RaycastHit forwardHit;
+    public RaycastHit heightHit;
 }
